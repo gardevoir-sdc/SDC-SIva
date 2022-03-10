e913f48d3069a771a679efac91bcbf7e0ba180f0
sivaranjani <sivaaranjani.thangavel@gmail.com>
2022-03-10 10:07:00 -0800

    Changes made

--- server/index.js
+++ server/index.js
@@ -14,26 +14,21 @@ const {
   listAnswers,
   listQuestions
  } = require('./controllers');
-//  const addAnswer = require('./controllers/addAnswer.js');
-//  const   addQuestion
-//  = require('./controllers/addQuestion.js');
+
 
 app.use(express.json());
 // app.use(cors());
 app.use(morgan('dev'));
-//app.use('/api', router);
-// app.get('/qa/questions', getQuestion);
 
-// app.get('/qa/questions/:question_id/answers', getAnswers);
 
 app.put('/api/qa/answers/:answer_id/report', reportAnswer);
 app.put('/qa/answers/:answer_id/helpful', helpfulAnswer);
 app.put('/qa/questions/:question_id/helpful', helpfulQuestion);
 app.put('/api/qa/questions/:question_id/report', reportQuestion);
-app.post('api/qa/questions/:question_id/answers', addAnswer);
-app.post('api/qa/questions', addQuestion);
-app.get('/qa/questions/:question_id/answers', listAnswers);
-app.get('/qa/questions', listQuestions);
+app.post('/api/qa/questions/:question_id/answers', addAnswer);
+app.post('/api/qa/questions', addQuestion);
+app.get('/api/qa/questions/:question_id/answers', listAnswers);
+app.get('/api/qa/questions', listQuestions);
 app.listen(port, () => {
   console.log(`Listening to port ${port}`)
 });
